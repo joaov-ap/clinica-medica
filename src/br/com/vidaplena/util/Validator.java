9466ab91369e2439d7d4c12961888450f75d9079
joaov-ap <vitor.1943@gmail.com>
2025-04-14 14:48:35 -0300

    refactor: two functions into one to valid cpf digit

--- src/br/com/vidaplena/util/Validator.java
+++ src/br/com/vidaplena/util/Validator.java
@@ -15,7 +15,7 @@ public class Validator {
 
         String cleanCpf = cpf.replaceAll("[.-]+", "");
         String[] splitedCpf = cleanCpf.split("");
-        return cpfFirstDigit(splitedCpf) == Integer.parseInt(splitedCpf[9]) && cpfSecondDigit(splitedCpf) == Integer.parseInt(splitedCpf[10]);
+        return cpfDigit(splitedCpf, 1) == Integer.parseInt(splitedCpf[9]) && cpfDigit(splitedCpf, 2) == Integer.parseInt(splitedCpf[10]);
     };
 
     public static boolean isEmailValid(String email) {
@@ -25,34 +25,26 @@ public class Validator {
         return matches;
     }
 
-    private static int cpfFirstDigit(String[] splitedCpf) {
-        int secondDigit = Integer.valueOf(splitedCpf[0]) * 10 + Integer.valueOf(splitedCpf[1]) * 9 + Integer.valueOf(splitedCpf[2]) * 8 +
-                Integer.valueOf(splitedCpf[3]) * 7 + Integer.valueOf(splitedCpf[4]) * 6 + Integer.valueOf(splitedCpf[5]) * 5 + Integer.valueOf(splitedCpf[6]) * 4 +
-                Integer.valueOf(splitedCpf[7]) * 3 + Integer.valueOf(splitedCpf[8]) * 2;
-
-        secondDigit %= 11;
-        secondDigit = 11 - secondDigit;
-
-        if (secondDigit >= 10) {
-            secondDigit = 0;
+    private static int cpfDigit(String[] splitedCpf, int value) {
+        int digit = 0;
+        if (value == 1) {
+            digit = Integer.valueOf(splitedCpf[0]) * 10 + Integer.valueOf(splitedCpf[1]) * 9 + Integer.valueOf(splitedCpf[2]) * 8 +
+                    Integer.valueOf(splitedCpf[3]) * 7 + Integer.valueOf(splitedCpf[4]) * 6 + Integer.valueOf(splitedCpf[5]) * 5 + Integer.valueOf(splitedCpf[6]) * 4 +
+                    Integer.valueOf(splitedCpf[7]) * 3 + Integer.valueOf(splitedCpf[8]) * 2;
+        } else if (value == 2) {
+            digit = Integer.valueOf(splitedCpf[0]) * 11 + Integer.valueOf(splitedCpf[1]) * 10 + Integer.valueOf(splitedCpf[2]) * 9 +
+                    Integer.valueOf(splitedCpf[3]) * 8 + Integer.valueOf(splitedCpf[4]) * 7 + Integer.valueOf(splitedCpf[5]) * 6 + Integer.valueOf(splitedCpf[6]) * 5 +
+                    Integer.valueOf(splitedCpf[7]) * 4 + Integer.valueOf(splitedCpf[8]) * 3 + Integer.valueOf(splitedCpf[9]) * 2;
         }
 
-        return secondDigit;
-    };
+        digit %= 11;
+        digit = 11 - digit;
 
-    private static int cpfSecondDigit(String[] splitedCpf) {
-        int secondDigit = Integer.valueOf(splitedCpf[0]) * 11 + Integer.valueOf(splitedCpf[1]) * 10 + Integer.valueOf(splitedCpf[2]) * 9 +
-                Integer.valueOf(splitedCpf[3]) * 8 + Integer.valueOf(splitedCpf[4]) * 7 + Integer.valueOf(splitedCpf[5]) * 6 + Integer.valueOf(splitedCpf[6]) * 5 +
-                Integer.valueOf(splitedCpf[7]) * 4 + Integer.valueOf(splitedCpf[8]) * 3 + Integer.valueOf(splitedCpf[9]) * 2;
-
-        secondDigit %= 11;
-        secondDigit = 11 - secondDigit;
-
-        if (secondDigit >= 10) {
-            secondDigit = 0;
+        if (digit >= 10) {
+            digit = 0;
         }
 
-        return secondDigit;
+        return digit;
     };
 
 }
